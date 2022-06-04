637ab93b1fc1729ac33059110131ef6fd50dee93
lani <laniiiiworld@gmail.com>
2022-06-04 16:26:54 +0900

    youTube Api 활용하여 데이터 조회

--- src/main.js
+++ src/main.js
@@ -1,47 +1,104 @@
 'use strict';
 
-const upNextContainer = document.querySelector('.upNext ul');
-
 const SERVERLESS_URL = 'https://hide-with-serverless-functions.netlify.app/';
 const VIDEO_URL = `${SERVERLESS_URL}youtube/v3/videos?`;
 const CHANNEL_URL = `${SERVERLESS_URL}youtube/v3/channels?`;
+const EMBED_URL = 'https://www.youtube.com/embed/';
 
-fetch(
-  VIDEO_URL +
-    new URLSearchParams({
+//API에서 동영상 정보를 가져오는 함수
+const getVideoInfo = (videoId) => {
+  let obj = {};
+  if (videoId) {
+    obj = {
+      part: 'snippet',
+      id: videoId,
+    };
+  } else {
+    obj = {
       part: 'snippet',
       chart: 'mostPopular',
       maxResult: 1,
       regionCode: 'KR',
-    })
-)
-  .then((res) => res.json())
-  .then((data) => {
-    data.items.forEach((item) => {
-      getChannelInfo(item);
-    });
-  })
-  .catch((err) => console.log(err));
+    };
+  }
 
-const getChannelInfo = (video) => {
+  fetch(VIDEO_URL + new URLSearchParams(obj))
+    .then((res) => res.json())
+    .then((data) => {
+      if (videoId) {
+        console.log('getVideoInfo ---> ', data);
+        //비디오가 선택되었을 때, 하나만 조회하는 경우
+        setVideoInfo(data.items[0]);
+        getChannelInfo(data.items[0], true);
+      } else {
+        //추천 동영상 목록에 보여줄 videos
+        data.items.forEach((item) => {
+          getChannelInfo(item);
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+//동영상 정보 세팅
+const setVideoInfo = (video) => {
+  console.log('setVideoInfo ---> ', video);
+  const videoPlayer = document.querySelector('.videoPlayer iframe');
+  const videoTitle = document.querySelector('.videoInfo .title');
+  const description = document.querySelector('.videoInfo .description');
+  const hitsAndDays = document.querySelector('.videoInfo .hitsAndDays');
+  const tags = document.querySelector('.videoInfo .info__tags');
+  videoPlayer.src = EMBED_URL + video.id;
+  videoTitle.innerHTML = video.snippet.title;
+  description.innerHTML = video.snippet.description;
+  hitsAndDays.innerHTML = '';
+  tags.innerHTML = ''; //video.snippet.tags
+  const channelName = document.querySelector('.channel .name');
+  channelName.innerHTML = video.snippet.channelTitle;
+};
+
+//API에서 채널 정보를 가져오는 함수
+const getChannelInfo = (video, isSetChannel) => {
   fetch(
     CHANNEL_URL +
       new URLSearchParams({
-        part: 'snippet',
+        part: 'snippet,statistics',
         id: video.snippet.channelId,
       })
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      video.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-      makeUpNexts(video);
+      if (isSetChannel) {
+        setChannelInfo(data.items[0]);
+      } else {
+        video.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeUpNexts(video);
+      }
     });
 };
 
+//채널 정보 세팅
+const setChannelInfo = (data) => {
+  console.log(`setChannelInfo --->`, data);
+  const user = document.querySelector('.channel .user');
+  const subscribers = document.querySelector('.channel .subscribers');
+  const subscriberCount = data.statistics.subscriberCount;
+  user.src = data.snippet.thumbnails.default.url;
+  if (subscriberCount.length > 5) {
+    subscribers.innerHTML = Math.floor((subscriberCount * 1) / 10000) + '만명';
+  } else if (subscriberCount.length > 4) {
+    subscribers.innerHTML =
+      Math.floor((subscriberCount * 1) / 1000) / 10 + '만명';
+  } else {
+    subscribers.innerHTML = subscriberCount;
+  }
+};
+
+//추천 동영상 목록을 만드는 함수
 const makeUpNexts = (data) => {
+  const upNextContainer = document.querySelector('.upNext ul');
   upNextContainer.innerHTML += `
-  <li class="next" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+  <li class="next" onclick="getVideoInfo('${data.id}');">
     <div class="img"><img src="${data.snippet.thumbnails.high.url}" alt="" /></div>
     <div class="info">
       <span class="title">${data.snippet.title}</span>
@@ -51,3 +108,10 @@ const makeUpNexts = (data) => {
   </li>
   `;
 };
+
+//onLoad
+const loadDetail = () => {
+  getVideoInfo();
+};
+
+loadDetail();
